9a766da5a88ce056e8f81fe2ff03c960dc1c349c
Kyyuserbot <83812990+Rifkiarisman@users.noreply.github.com>
2021-08-13 03:38:42 +0700

    Update create.py

--- Userbot/Modules/create.py
+++ Userbot/Modules/create.py
@@ -47,7 +47,7 @@ async def telegraphs(grop):
                 result = await grop.client(functions.messages.ExportChatInviteRequest(
                     peer=created_chat_id,
                 ))
-                await grop.edit("King, Grup/Channel {} Sukses Dibuat. Tekan [{}]({}) Untuk Melihatnya".format(group_name, group_name, result.link))
+                await grop.edit("Yeaay, Grup/Channel {} Sukses Dibuat. Tekan [{}]({}) Untuk Melihatnya".format(group_name, group_name, result.link))
             except Exception as e:  # pylint:disable=C0103,W0703
                 await grop.edit(str(e))
 
