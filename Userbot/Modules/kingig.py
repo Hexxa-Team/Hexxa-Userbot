5a2ff719da4ad538624df37df759cac5b9ec9d9a
Kyyuserbot <83812990+Rifkiarisman@users.noreply.github.com>
2021-08-13 04:10:19 +0700

    Update kingig.py

--- Userbot/Modules/kingig.py
+++ Userbot/Modules/kingig.py
@@ -1,15 +1,9 @@
-# Koala Ganteng, Kode Dari Koala Bangsul Press F untuk Koala @Manusiarakitann
-# Keredit Motor Eh Maksudnya Kredit Kampang Bot (c) Koala Bgke @ManusiaRakitann
-# Karna Aku Gabut Aku Pasang Keredit Lagi # Keredit
-# Yak Pasang Credit Banyak Banyak Biar Makin Keren
-# Copyright (C) 2021 Apis / @PacarFerdilla By King Userbot
+# Copyright (C) 2021 Kyy / @sokapgblg By Hexxa Userbot
 # All rights reserved.
 # Keredit
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
-# King Userbot - From King To King
-# Yang Gbs Basa Enggres bisa Terjemahkan di atas
-# Ngefork Doang Gak Bintang Anjg
+# Hexxa Userbot
 # Kalo Clone Ini Jangan dihapus ya anjg nanti Apis Ngamuk, Ok Mksh Sma Sma
 
 from telethon import events
@@ -17,20 +11,17 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from userbot.events import register
 from userbot import bot, CMD_HELP
 
-# Apis Gans
-# Apin Gansssss Anjjjayy Yahahaha
-
 
 @register(outgoing=True, pattern="^.ig ?(.*)")
 async def insta(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.edit("`King, Mohon Balas Ke Link Instagram`")
+        await event.edit("`Maaf, Mohon Balas Ke Link Instagram`")
         return
     reply_message = await event.get_reply_message()
     if not reply_message.text:
-        await event.edit("`Mohon Maaf King, Saya Membutuhkan Link Media Instagram Untuk Download`")
+        await event.edit("`Mohon Maaf, Saya Membutuhkan Link Media Instagram Untuk Download`")
         return
     chat = "@SaveAsBot"
     reply_message.sender
@@ -46,7 +37,7 @@ async def insta(event):
             await event.client.send_message(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await event.edit("`King, Mohon Buka Blokir` @SaveAsbot `Lalu Coba Lagi`")
+            await event.edit("`Maaf, Mohon Buka Blokir` @SaveAsbot `Lalu Coba Lagi`")
             return
         if response.text.startswith("Forward"):
             await event.edit(
@@ -57,16 +48,13 @@ async def insta(event):
             await event.client.send_file(
                 event.chat_id,
                 response.message.media,
-                caption=f"**Download By @KingUserbotSupport**",
+                caption=f"**Download By Hexxa-Userbot**",
             )
             await event.client.send_read_acknowledge(conv.chat_id)
             await bot(functions.messages.DeleteHistoryRequest(peer=chat, max_id=0))
             await event.delete()
 
 
-# By King - Userbot
-# Apis Gansssssss Mksh Sma Sma
-# Apis Gans
 CMD_HELP.update(
     {
         "instagram": "**✘ Plugin :** __Instagram__\
