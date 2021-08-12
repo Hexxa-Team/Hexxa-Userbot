b652b07630aa07c0a59ea2db838e6fe8d3a5ed3b
Kyyuserbot <83812990+Rifkiarisman@users.noreply.github.com>
2021-08-13 04:43:11 +0700

    Update __main__.py

--- Userbot/__main__.py
+++ Userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⚡King-Userbot⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"Hexxa-Userbot🔥 ⚙️ V{BOT_VER} [TELAH AKTIF!!]")
 
 
 if len(argv) not in (1, 3, 4):
