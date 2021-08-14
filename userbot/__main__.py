3162a27904a9893b88673f48a6134bb9fb12c740
KyyProject <83812990+Rifkiarisman@users.noreply.github.com>
2021-08-15 03:48:18 +0700

    Add files via upload

---
+++ userbot/__main__.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot start point """
+
+from importlib import import_module
+from sys import argv
+
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+from userbot import BOT_VER, LOGS, bot
+from userbot.modules import ALL_MODULES
+
+INVALID_PH = '\nERROR : Nomor Telepon yang Anda dimasukkan Tidak Valid' \
+             '\n Tips : Gunakan Kode Negara beserta nomornya dengan benar' \
+             '\n atau periksa nomor telepon Anda dan coba lagi !'
+
+try:
+    bot.start()
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    exit(1)
+
+for module_name in ALL_MODULES:
+    imported_module = import_module("userbot.modules." + module_name)
+
+
+LOGS.info(
+    f"Hexxa-Userbot🔥 ⚙️ V{BOT_VER} [TELAH AKTIF!!]")
+
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
