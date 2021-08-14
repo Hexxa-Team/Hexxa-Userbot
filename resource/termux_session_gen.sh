b2a7baef47801436c7654a9c55e87e767216f0b6
KyyProject <83812990+Rifkiarisman@users.noreply.github.com>
2021-08-15 02:14:56 +0700

    fix

---
+++ resource/termux_session_gen.sh
@@ -0,0 +1,22 @@
+# ------------------------------------------------STRING-TERMUX------------------------------------------------------------>
+clear
+echo "
+██╗░░██╗██╗███╗░░██╗░██████╗░
+██║░██╔╝██║████╗░██║██╔════╝░
+█████═╝░██║██╔██╗██║██║░░██╗░
+██╔═██╗░██║██║╚████║██║░░╚██╗
+██║░╚██╗██║██║░╚███║╚██████╔╝
+╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░
+"
+# Start deploying...
+echo Starting dependency installation in 5 seconds...
+sleep 5
+apt-get update
+apt-get upgrade -y
+pkg upgrade -y
+pkg install python wget -y
+wget https://raw.githubusercontent.com/apisuserbot/King-Userbot/King-Userbot/resource/king-startup.py
+pip install telethon
+python king-startup.py
+
+# -------------------------------------------------END-LOG---------------------------------------------------------- #
